4ef810124991eb0872d7224b35ebe1cd4d2e56a3
PfMartin <martinpfatrisch@gmail.com>
2025-08-11 21:08:21 +0200

    Added resources and resource template for the users example

--- server.ts
+++ server.ts
@@ -1,7 +1,18 @@
-import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import {
+  McpServer,
+  ResourceTemplate,
+} from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 import { z } from 'zod';
 import fs from 'node:fs/promises';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+}
 
 const server = new McpServer({
   name: 'Test',
@@ -12,6 +23,74 @@ const server = new McpServer({
     prompts: {},
   },
 });
+
+server.resource(
+  'users',
+  'users://all',
+  {
+    description: 'Get all users data from the database',
+    title: 'Users',
+    mimeType: 'application/json',
+  },
+  async (uri) => {
+    const usersFilePath = './data/users.json';
+    const users = await import(usersFilePath, {
+      with: { type: 'json' },
+    }).then((m) => m.default);
+
+    return {
+      contents: [
+        {
+          uri: uri.href,
+          text: JSON.stringify(users),
+          mimeType: 'application/json',
+        },
+      ],
+    };
+  },
+);
+
+server.resource(
+  'user-details',
+  new ResourceTemplate('users://{userId}/profile', { list: undefined }),
+  {
+    description: 'Get a users details from the database',
+    title: 'Users Details',
+    mimeType: 'application/json',
+  },
+  async (uri, { userId }) => {
+    const usersFilePath = './data/users.json';
+    const users = await import(usersFilePath, {
+      with: { type: 'json' },
+    }).then((m) => m.default);
+
+    const user = users.find((u: User) => u.id === parseInt(userId as string));
+
+    if (!user) {
+      return {
+        contents: [
+          {
+            uri: uri.href,
+            text: JSON.stringify({
+              error: `Could not find user with id ${userId}`,
+            }),
+            mimeType: 'application/json',
+          },
+        ],
+      };
+    }
+
+    return {
+      contents: [
+        {
+          uri: uri.href,
+          text: JSON.stringify(user),
+          mimeType: 'application/json',
+        },
+      ],
+    };
+  },
+);
 
 server.tool(
   'create-user',
